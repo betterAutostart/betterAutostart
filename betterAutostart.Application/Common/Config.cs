4d7c75dbb35df328cf3636564373f86663e7cf9b
Erijl <justusmau@web.de>
2022-06-17 16:39:20 +0200

    rename root directory

---
+++ betterAutostart.Application/Common/Config.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using betterAutostart.Application.Common.ProfileClasses;
+using betterAutostart.Application.Common.Translation;
+using betterAutostart.Application.Model;
+
+namespace betterAutostart.Application.Common
+{
+    class Config
+    {
+        /// <summary>
+        /// The global currently selected language 
+        /// </summary>
+        public static LanguageObj ActiveLanguage;
+        
+        /// <summary>
+        /// Main Form displaying childForms
+        /// </summary>
+        public static application ApplicationForm;
+        
+        /// <summary>
+        /// Global ProfileHandler
+        /// </summary>
+        public static ProfileHandler PHandler;
+        
+        /// <summary>
+        /// Global SaveSystem
+        /// </summary>
+        public static SaveSystem SaveSystem;
+        
+        /// <summary>
+        /// Global ErrorLog
+        /// </summary>
+        public static ErrorLog ErrorLog;
+
+        /*
+         * Used packages:
+         * Newtonsoft.Json (https://www.newtonsoft.com/json)
+         * NHotkey.WindowsForms (https://github.com/thomaslevesque/NHotkey)
+         * Microsoft Visual Studio Installer Projects (https://marketplace.visualstudio.com/items?itemName=VisualStudioClient.MicrosoftVisualStudio2017InstallerProjects)
+         */
+
+        /// <summary>
+        /// Instantiate Global objects
+        /// </summary>
+        public static void InstantiateConfig()
+        {
+            LanguageSupport.LoadAllLanguages();
+            ActiveLanguage = LanguageSupport.GetLanguageByName(Properties.Settings.Default["SelectedLanguage"].ToString());
+            PHandler = new ProfileHandler();
+            ErrorLog = new ErrorLog();
+            SaveSystem = new SaveSystem();
+        }
+    }
+}
---
+++ betterAutostart.Application/View/application.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using betterAutostart.Application.Common;
+using NHotkey.WindowsForms;
+using NHotkey;
+
+namespace betterAutostart.Application
+{
+    public partial class application : Form
+    {
+        private Form activeForm = null;
+        
+        public application()
+        {
+            #if DEBUG
+            Utility.DesignMode = true;
+            #endif
+
+            Config.InstantiateConfig();
+            Config.ApplicationForm = this;
+            InitializeComponent();
+            this.Text = "BetterAutostart";
+
+            this.UpdateTranslation();
+            Region = Region.FromHrgn(Utility.CreateRoundRectRgn(0, 0, Width, Height, 6, 6));
+            this.btn_sideMenu_profiles_Click((this.btn_sideMenu_profiles as object), null);
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        public void UpdateTranslation() 
+        {
+            btn_sideMenu_profiles.Text = Config.ActiveLanguage.Strings.SideMenuProfiles;
+            btn_sideMenu_website.Text = Config.ActiveLanguage.Strings.SideMenuWebsite;
+            btn_sideMenu_wiki.Text = Config.ActiveLanguage.Strings.SideMenuWiki;
+            btn_sideMenu_srccode.Text = Config.ActiveLanguage.Strings.SideMenuSourceCode;
+
+            btn_sideMenu_settings.Text = Config.ActiveLanguage.Strings.SideMenuSettings;
+        }
+
+        private void btn_sideMenu_profiles_Click(object sender, EventArgs e)
+        {
+            this.HighlightNav(sender);
+            this.OpenChildForm(new profilePanel());
+            this.lbl_childFormHeader.Text = Config.ActiveLanguage.Strings.SideMenuProfiles;
+        }
+
+        private void btn_sideMenu_website_Click(object sender, EventArgs e)
+        {
+            Utility.OpenWebsite("https://betterautostart.com");
+        }
+
+        private void btn_sideMenu_wiki_Click(object sender, EventArgs e)
+        {
+            Utility.OpenWebsite("https://betterautostart.com/wiki");
+        }
+
+        private void btn_sideMenu_srccode_Click(object sender, EventArgs e)
+        {
+            Utility.OpenWebsite("https://betterautostart.com/github");
+        }
+
+        private void btn_sideMenu_settings_Click(object sender, EventArgs e)
+        {
+            this.HighlightNav(sender);
+            this.OpenChildForm(new settings());
+            this.lbl_childFormHeader.Text = Config.ActiveLanguage.Strings.SideMenuSettings;
+        }
+
+        public void OpenAutostartProfilePanel()
+        {
+            this.btn_sideMenu_profiles_Click((this.btn_sideMenu_profiles as object), null);
+        }
+
+        public void HighlightNav(object entry)
+        {
+            if(entry == null)
+            {
+                pnl_navBarHighlight.Height = 0;
+                pnl_navBarHighlight.Top = 700;
+                pnl_navBarHighlight.Left = 0;
+            }
+            else
+            {
+                pnl_navBarHighlight.Height = (entry as Button).Height;
+                pnl_navBarHighlight.Top = (entry as Button).Top;
+                pnl_navBarHighlight.Left = (entry as Button).Left;
+            }
+        }
+
+        public void OpenChildForm(Form childForm)
+        {
+            if (activeForm != null && activeForm.GetType() == childForm.GetType())
+            {
+                return;
+            }
+
+            if(activeForm != null)
+            {
+                activeForm.Hide();
+            }
+
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+
+            pnl_childForm.Controls.Add(childForm);
+            pnl_childForm.Tag = childForm;
+            childForm.Show();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void application_Load(object sender, EventArgs e)
+        {
+            Utility.SetIcon(this);
+        }
+    }
+}
